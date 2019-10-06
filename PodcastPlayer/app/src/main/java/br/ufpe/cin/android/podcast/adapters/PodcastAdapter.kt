222e632f91bd948481ebd9add0442d236d18a8d7
Rodrigo Carneiro <rodrigo.carneiro@inloco.com.br>
2019-10-06 18:54:46 -0300

    chore(pause_icon.png): Add icon

--- PodcastPlayer/app/src/main/java/br/ufpe/cin/android/podcast/adapters/PodcastAdapter.kt
+++ PodcastPlayer/app/src/main/java/br/ufpe/cin/android/podcast/adapters/PodcastAdapter.kt
@@ -5,16 +5,25 @@ import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import br.ufpe.cin.android.podcast.EpisodeDetailActivity
 import br.ufpe.cin.android.podcast.ItemFeed
 import br.ufpe.cin.android.podcast.MainActivity
 import br.ufpe.cin.android.podcast.R
 import br.ufpe.cin.android.podcast.services.DownloadService
+import br.ufpe.cin.android.podcast.services.PodcastPlayerService
 import kotlinx.android.synthetic.main.itemlista.view.*
 
-class PodcastAdapter (private var episodes: List<ItemFeed>, private val application: MainActivity):
+class PodcastAdapter (private var episodes: List<ItemFeed>,
+                      private val application: MainActivity):
     RecyclerView.Adapter<PodcastAdapter.ViewHolder>() {
+    var isServiceBound: Boolean = false
+
+    private var podcastPlayerService: PodcastPlayerService? = null
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val episodeView =
             LayoutInflater
@@ -29,13 +38,24 @@ class PodcastAdapter (private var episodes: List<ItemFeed>, private val applicat
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val episode = episodes[position]
 
-        holder.title?.text = episode.title
-        holder.pubDate?.text = episode.pubDate
+        holder.title.text = episode.title
+        holder.pubDate.text = episode.pubDate
 
         setDownloadButtonVisibility(holder, episode)
 
         setOnItemViewClickListener(holder, episode)
         setOnDownloadButtonClickListener(holder, episode)
+        setOnPlayButtonClickListener(holder, episode)
+    }
+
+    // Pass a new list of episodes and notify
+    fun updateEpisodeList(episodeList: List<ItemFeed>) {
+        this.episodes = episodeList
+        notifyItemInserted(episodes.size)
+    }
+
+    fun setPodCastService(podcastPlayerService: PodcastPlayerService) {
+        this.podcastPlayerService = podcastPlayerService
     }
 
     private fun setDownloadButtonVisibility(holder: ViewHolder, episode: ItemFeed) {
@@ -66,15 +86,25 @@ class PodcastAdapter (private var episodes: List<ItemFeed>, private val applicat
         }
     }
 
-    // Pass a new list of episodes and notify
-    fun updateEpisodeList(episodeList: List<ItemFeed>) {
-        this.episodes = episodeList
-        notifyItemInserted(episodes.size)
+    private fun setOnPlayButtonClickListener(holder: ViewHolder, episode: ItemFeed) {
+        holder.playButton.setOnClickListener {
+            if (isServiceBound) {
+                if (!podcastPlayerService!!.isPlaying()) {
+                    podcastPlayerService!!.setPodCastDataSource(episode.episodePath!!)
+                    podcastPlayerService!!.playMusic()
+                    holder.itemView.item_play_action.setImageResource(R.drawable.pause_icon)
+                } else {
+                    podcastPlayerService!!.pauseMusic()
+                    holder.itemView.item_play_action.setImageResource(R.drawable.play_icon)
+                }
+            }
+        }
     }
 
     class ViewHolder (episodeView : View) : RecyclerView.ViewHolder(episodeView) {
-        val title = episodeView.item_title
-        val pubDate = episodeView.item_date
-        val downloadButton = episodeView.item_action
+        val title: TextView = episodeView.item_title
+        val pubDate: TextView = episodeView.item_date
+        val downloadButton: Button = episodeView.item_action
+        val playButton: ImageButton = episodeView.item_play_action
     }
 }
